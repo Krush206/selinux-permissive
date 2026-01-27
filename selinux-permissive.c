d930dbf5b198798a9ef29c541162fdc86138580a
Matheus Garcia <tateusg@protonmail.com>
2026-01-26 21:54:24 -0300

    Minor update

--- selinux-permissive.c
+++ selinux-permissive.c
@@ -5,20 +5,23 @@
 MODULE_LICENSE("BSD");
 MODULE_AUTHOR("Matheus Garcia");
 MODULE_DESCRIPTION("Disable SELinux enforcing.");
-MODULE_VERSION("0.1");
+MODULE_VERSION("0.1.1");
 
 static int *selinux_enforcing;
 static void (*selnl_notify_setenforce)(int);
 static void (*selinux_status_update_setenforce)(int);
+static int (*avc_ss_reset)(u32);
 static struct task_struct *thread;
 
 static unsigned long enforcing_addr;
 static unsigned long notify_addr;
 static unsigned long status_update_addr;
+static unsigned long ss_reset_addr;
 
 module_param(enforcing_addr, ulong, 0644);
 module_param(notify_addr, ulong, 0644);
 module_param(status_update_addr, ulong, 0644);
+module_param(ss_reset_addr, ulong, 0644);
 
 static int selinux_permissive(void *data) {
     while(!kthread_should_stop())
@@ -39,12 +42,14 @@ static int __init selinux_permissive_start(void)
 {
     if(!enforcing_addr ||
        !notify_addr ||
-       !status_update_addr)
+       !status_update_addr ||
+       !ss_reset_addr)
         return -EINVAL;
 
     selinux_enforcing = (void *) enforcing_addr;
     selnl_notify_setenforce = (void *) notify_addr;
     selinux_status_update_setenforce = (void *) status_update_addr;
+    avc_ss_reset = (void *) ss_reset_addr;
     thread = kthread_run(selinux_permissive, NULL, "selinux_permissive");
 
     return 0;
@@ -54,6 +59,7 @@ static void __exit selinux_permissive_stop(void)
 {
     kthread_stop(thread);
     WRITE_ONCE(*selinux_enforcing, 1);
+    avc_ss_reset(0);
     selnl_notify_setenforce(*selinux_enforcing);
     selinux_status_update_setenforce(*selinux_enforcing);
 }
